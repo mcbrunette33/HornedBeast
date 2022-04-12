329a8f642aa417bf8e575e09f79d1a1a1de42acc
michael <mcbrunette33@gmail.com>
2022-04-11 18:11:13 -0700

    added lab 01

---
+++ src/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+class Footer extends React.Component {
+  render() {
+    return <h1>Author: Michael Brunette</h1>;
+  }
+}
+module.exports.Footer = Footer;
---
+++ src/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+class Header extends React.Component {
+  render() {
+    return <h1>Hello, <title>lab-01</title></h1>;
+  }
+}
+module.exports.Header = Header;
